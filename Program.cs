ba109c04386cedfa76b4e30b2aed8b9c1e8282eb
Stoyan Kolev <stoyan.a.kolev@gmail.com>
2024-08-26 21:07:00 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/inventory", ([FromQuery] string[] tags) => tags);
+
+app.Run();
